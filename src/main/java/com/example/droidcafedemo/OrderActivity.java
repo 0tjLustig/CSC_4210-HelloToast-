2b451b753dbf16de219fd9ec43797211ff970f3b
0tjLustig <55846769+0tjLustig@users.noreply.github.com>
2020-02-13 10:26:17 -0600

    files added for 4.1

---
+++ src/main/java/com/example/droidcafedemo/OrderActivity.java
@@ -0,0 +1,32 @@
+package com.example.droidcafedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Intent intent = getIntent();
+        String message = "Order: " +
+                intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.order_textview);
+        textView.setText(message);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        if(spinner != null){
+            spinner.setAdapter(adapter);
+        }
+    }
+}
